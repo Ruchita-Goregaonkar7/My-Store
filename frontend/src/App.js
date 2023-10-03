be6d17310ce3f0e72f6c8063548dbc2b6bc7cc9a
Ruchita Goregaonkar <ruchitag98@gmail.com>
2023-10-03 19:31:47 -0400

    step 4 - Create HomeScreen product listings done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
+import { Flex } from "@chakra-ui/react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () =>{
     return(
+        // <div>
+        //     <h1>My App</h1>
+        //     <Header/>
+        //     <div style={{height:'100px'}}></div>
+        //     <Footer/>
+        // </div>
         <div>
-            <h1>My App</h1>
             <Header/>
-            <div style={{height:'100px'}}></div>
+            <Flex
+                as='main'
+                mt='72px'
+                direction='column'
+                py='6'
+                px='6'
+                bgColor='gray.200'>
+                <HomeScreen/>
+            </Flex>
             <Footer/>
         </div>
     );
